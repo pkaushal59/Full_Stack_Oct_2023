4daebcf7f9388991ee55dfd245c20980240589ef
Ashwani Rajput <ashwin.rajput87@gmail.com>
2023-12-04 23:39:40 +0530

    Lec-7 - application of closures, creation of objects completed

---
+++ JavaScript/Lec-7_application_of_closure_object_creations_freezing_objects/quiz-3.js
@@ -0,0 +1,13 @@
+function outer() {
+    let arrFn = [];
+    for (var i=0;i<3;i++) {
+        arrFn.push(function fn() {
+            console.log(i);
+        })
+    }
+    return arrFn;
+}
+let arrFn = outer();
+arrFn[0]();
+arrFn[1]();
+arrFn[2]();
