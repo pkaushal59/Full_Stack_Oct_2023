e5232dcc0d40c886a9e0a5d51c9df16e67bd683f
Ashwani Rajput <ashwin.rajput87@gmail.com>
2023-12-27 23:47:14 +0530

    Lec-4 - FEMC - countdown timer completed and nested comment box few functionalities are pending

--- FE-Machine-Coding/Lec-3_Mini_Projects/mini-project/countDownTimer/js/script.js
+++ FE-Machine-Coding/Lec-3_Mini_Projects/mini-project/countDownTimer/js/script.js
@@ -4,7 +4,7 @@
  *   must have features:
  *      pass values: in mins and seconds input fields
  *      play, stop, reset buttons
- *      validtaion on the input fileds are required
+ *      validtaion on the input fields are required
  * 
  *  good to have features:
  *     reset the timer
@@ -55,6 +55,9 @@ class Timer {
 
     onStart() {
         console.log("start the timer");
+        this.display.classList.add('progress');
+        this.setInputDisabledSatus(true);
+        this.setControls(true, false);
         this.setTime();
         this.startTimer();
     }
@@ -67,18 +70,32 @@ class Timer {
     }
 
     startTimer(){
-        this.intervalId = setInterval(()=>{
-           this.seconds-=1;
 
-           if(this.seconds<0) {
+        if(this.minutes === 0 && this.seconds === 0){
+            // this.onReset();
+            return
+        }
+
+        this.intervalId = setInterval(()=>{ // 1,2,3 ,4 ... last timerID
+            if(this.minutes === 0 && this.seconds === 0){
+                this.onReset();
+            }
+           
+            this.seconds-=1;
+
+            if(this.seconds<0) {
              this.seconds = 59;
              this.minutes-=1;
-           }
-
+            }
            this.setDisplay(this.minutes, this.seconds);
         }, 1000);
     }
 
+    /**
+     * Method is used for enable/disable the input display
+     * @param {for minutes minMSB, minLSB} mins 
+     * @param {for seconds secMSB, secLSB} secs 
+     */
     setDisplay(mins, secs) {
      this.minMSB.value = String(Math.floor(mins/10));
      this.minLSB.value = String(mins%10);
@@ -86,16 +103,64 @@ class Timer {
      this.secLSB.value = String(Math.floor(secs%10));
     }
 
+    setInputDisabledSatus(isDisabled=false) {
+        this.minMSB.disabled = isDisabled;
+        this.minLSB.disabled = isDisabled;
+        this.secMSB.disabled = isDisabled;
+        this.secLSB.disabled = isDisabled;
+    }
+
+    setControls(startStaus=false, stopStaus=false) {
+        this.start.disabled = startStaus;
+        this.stop.disabled = stopStaus;
+    }
+
     onStop() {
-
+        this.display.classList.remove('progress');
+        this.setInputDisabledSatus(false);
+        this.setControls(false, true);
+        clearInterval(this.intervalId);
     }
 
-    onReset() {
-
+    onReset() { // Please do it in your home work
+      this.resetTimerValue();
+      clearInterval(this.intervalId);
     }
 
-    onInput() {
+    resetTimerValue(){
+        this.minMSB.value = 0;
+        this.minLSB.value = 0;
+        this.secMSB.value = 0;
+        this.secLSB.value = 0;
+    }
 
+    onInput(event) {
+        const value = Number(event.data);
+
+        if(Number.isInteger(value)){
+            if(this.minMSB === document.activeElement){
+                this.onValueEntry(this.minMSB, this.minLSB, value, 0, 5);
+            } else if(this.minLSB === document.activeElement) {
+                this.onValueEntry(this.minLSB, this.secMSB, value, 0, 9);
+            } else if(this.secMSB === document.activeElement) {
+                this.onValueEntry(this.secMSB, this.secLSB, value, 0, 5);
+            } else if (this.secLSB === document.activeElement) {
+                this.onValueEntry(this.secLSB, null,value,  0, 9);
+            }
+        }
+    }
+
+    onValueEntry(target, nextTarget, value, minAllowed, maxAllowed) {
+        if(value>=minAllowed && value<= maxAllowed) {
+            target.value = value;
+            if(nextTarget) {
+                nextTarget.focus();
+                nextTarget.select();
+            } else {
+                target.value = 0;
+                target.select();
+            }
+        }
     }
 }
 
